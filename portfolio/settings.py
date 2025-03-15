a40c13976067c34e12d9c554abd48aa90f899faa
Shiloh <shilohe.ai@gmail.com>
2025-03-15 21:46:15 +0100

    Updated Settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -21,12 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--r-6v00q-kec5sp&(6$j#w$$*155aw!8bp)xlz530(6=x_kl7$'
+# SECURITY SETTINGS
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='your-default-secret-key')
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 OPENAI_API_KEY = config('OPENAI_API_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
