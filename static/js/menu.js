52866f895382e51f4d1143528419f5abf7e2357e
Shiloh <shilohe.ai@gmail.com>
2025-06-13 09:08:57 +0100

    Updated header logic and integrated alpine.js using theme app

--- static/js/menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const mobileMenu = document.getElementById('mobileMenu');
-
-    menuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden');
-        menuToggle.classList.toggle('open');
-    });
-});
